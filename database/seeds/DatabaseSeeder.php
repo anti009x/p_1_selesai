53b5afe233be99795799ddc60c717255d1ce606b
anti009x <riskisuleman76@gmail.com>
2023-06-10 14:35:35 +0700

    Menambahkan file .env dan file database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,32 +19,32 @@ class DatabaseSeeder extends Seeder
 
         $user = User::create([
             'name'          => 'Admin',
-            'email'         => 'admin@mail.com',
-            'password'      => bcrypt('codeastro.com'),
+            'email'         => 'riskisuleman76@gmail.com',
+            'password'      => bcrypt('sabun12345ke'),
             'created_at'    => date("Y-m-d H:i:s")
         ]);
         $user->assignRole('Admin');
 
         $user2 = User::create([
             'name'          => 'Teacher',
-            'email'         => 'teacher@mail.com',
-            'password'      => bcrypt('codeastro.com'),
+            'email'         => 'teddy@gmail.com',
+            'password'      => bcrypt('sabun12345ke'),
             'created_at'    => date("Y-m-d H:i:s")
         ]);
         $user2->assignRole('Teacher');
 
         $user3 = User::create([
             'name'          => 'Parent',
-            'email'         => 'parent@mail.com',
-            'password'      => bcrypt('codeastro.com'),
+            'email'         => 'anwar@gmail.com',
+            'password'      => bcrypt('sabun12345ke'),
             'created_at'    => date("Y-m-d H:i:s")
         ]);
         $user3->assignRole('Parent');
 
         $user4 = User::create([
             'name'          => 'Student',
-            'email'         => 'student@mail.com',
-            'password'      => bcrypt('codeastro.com'),
+            'email'         => 'gibran@gmail.com',
+            'password'      => bcrypt('sabun12345ke'),
             'created_at'    => date("Y-m-d H:i:s")
         ]);
         $user4->assignRole('Student');
